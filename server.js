73d4b7d52797cd96307b7ce6c2cb78a983f78ddb
Dax <limeside@gmail.com>
2018-03-01 13:24:47 -0600

    Add preinstall script for ng. :rabbit:

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
+//Install express server
 const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3001;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'dist')));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
 
-
-app.set('port', port);
-app.listen(port, () => console.log('listen'));
