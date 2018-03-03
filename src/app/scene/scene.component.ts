670643ad89b274166952e111fe9e7e915fd6af12
Dax <limeside@gmail.com>
2018-03-02 20:23:43 -0600

    Edit rotations for markers. :rabbit:

--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -49,8 +49,9 @@ export class SceneComponent implements OnInit, OnDestroy {
       const coords = this.mathService.topocentric_from_lla(lla.lat, lla.lng, lla.alt, 0, 0, 0);
       const mesh: MeshNode = new MeshNode(lla.lat, lla.lng, lla.alt, coords.x, coords.y, coords.z, marker);
 
-      mesh.marker.rotation.y = this.nodes[i].CameraPitch;
-      mesh.marker.rotation.z = this.getCameraYaw(this.nodes[i].CameraYaw);
+      mesh.marker.rotateY(this.mathService.degreeToRadian(this.nodes[i].CameraPitch));
+      mesh.marker.rotateX(this.mathService.degreeToRadian(this.getCameraYaw(this.nodes[i].CameraYaw)));
+      mesh.marker.rotateZ(0);
 
       meshes.push(mesh);
     }
@@ -163,7 +164,6 @@ export class SceneComponent implements OnInit, OnDestroy {
     if (String(yaw).charAt(0) === '+') {
       return Number(yaw.substr(1));
     }
-
     return Number(yaw);
   }
 }
